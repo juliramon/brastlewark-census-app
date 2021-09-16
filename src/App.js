781a7a92c4bc21b0112a4ca7bee71e831b7e6280
Juli Ramon <juli@nexumlabs.com>
2021-09-16 16:27:22 +0200

    Implement sort functionality on the filter bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import CensusService from "./services/censusService";
 import CensusListItem from "./components/CensusListItem";
-import "./App.css";
 import NavBar from "./components/NavBar";
 import FilterBar from "./components/FilterBar";
+import "./App.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 const App = () => {
   const service = new CensusService();
@@ -11,6 +13,7 @@ const App = () => {
   const [hasCensus, setHasCensus] = useState(false);
   const [census, setCensus] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [sortedData, setSortedData] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -54,7 +57,64 @@ const App = () => {
           />
         );
       });
+  } else {
+    censusList = (
+      <div className="loading-alert">
+        <Loader type="Oval" color="#00BFFF" height={50} width={50} />
+        <span className="loading-alert__message">Loading...</span>
+      </div>
+    );
   }
+
+  const sortData = (colTitle) => {
+    console.log("sort data");
+    console.log(colTitle);
+    if (sortedData === false || sortedData === "decremental") {
+      const sortedList = census
+        .sort((a, b) => {
+          if (
+            colTitle === "id" ||
+            colTitle === "age" ||
+            colTitle === "weight" ||
+            colTitle === "height"
+          ) {
+            return a[colTitle] - b[colTitle];
+          } else {
+            if (a[colTitle].toLowerCase() > b[colTitle].toLowerCase()) {
+              return -1;
+            }
+            if (a[colTitle].toLowerCase() < b[colTitle].toLowerCase()) {
+              return 1;
+            }
+            return 0;
+          }
+        })
+        .reverse();
+      setCensus(sortedList);
+      setSortedData("incremental");
+    } else if (sortedData === "incremental") {
+      const sortedList = census.sort((a, b) => {
+        if (
+          colTitle === "id" ||
+          colTitle === "age" ||
+          colTitle === "weight" ||
+          colTitle === "height"
+        ) {
+          return a[colTitle] - b[colTitle];
+        } else {
+          if (a[colTitle].toLowerCase() > b[colTitle].toLowerCase()) {
+            return -1;
+          }
+          if (a[colTitle].toLowerCase() < b[colTitle].toLowerCase()) {
+            return 1;
+          }
+          return 0;
+        }
+      });
+      setCensus(sortedList);
+      setSortedData("decremental");
+    }
+  };
 
   return (
     <>
@@ -63,7 +123,7 @@ const App = () => {
       </header>
       <main className="app-wrapper">
         <section className="census-list">
-          <FilterBar />
+          <FilterBar sortData={sortData} />
           <div className="census-list__wrapper">{censusList}</div>
         </section>
       </main>
--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,15 +1,115 @@
 import React from "react";
 
-const FilterBar = ({}) => {
+const FilterBar = ({ sortData }) => {
   return (
     <div className="filter__bar">
-      <div className="filter-bar__item id">Id</div>
+      <div className="filter-bar__item id">
+        <button onClick={() => sortData("id")}>
+          Id{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-arrows-sort"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 9l4 -4l4 4m-4 -4v14" />
+            <path d="M21 15l-4 4l-4 -4m4 4v-14" />
+          </svg>
+        </button>
+      </div>
       <div className="filter-bar__item thumbnail">Thumbnail</div>
-      <div className="filter-bar__item name">Name</div>
-      <div className="filter-bar__item gender">Gender</div>
-      <div className="filter-bar__item age">Age</div>
-      <div className="filter-bar__item weight">Weight</div>
-      <div className="filter-bar__item height">Height</div>
+      <div className="filter-bar__item name">
+        <button onClick={() => sortData("name")}>
+          Name{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-arrows-sort"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 9l4 -4l4 4m-4 -4v14" />
+            <path d="M21 15l-4 4l-4 -4m4 4v-14" />
+          </svg>
+        </button>
+      </div>
+      <div className="filter-bar__item gender">Gender </div>
+      <div className="filter-bar__item age">
+        <button onClick={() => sortData("age")}>
+          Age{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-arrows-sort"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 9l4 -4l4 4m-4 -4v14" />
+            <path d="M21 15l-4 4l-4 -4m4 4v-14" />
+          </svg>
+        </button>
+      </div>
+      <div className="filter-bar__item weight">
+        <button onClick={() => sortData("weight")}>
+          Weight{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-arrows-sort"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 9l4 -4l4 4m-4 -4v14" />
+            <path d="M21 15l-4 4l-4 -4m4 4v-14" />
+          </svg>
+        </button>
+      </div>
+      <div className="filter-bar__item height">
+        <button onClick={() => sortData("height")}>
+          Height{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-arrows-sort"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 9l4 -4l4 4m-4 -4v14" />
+            <path d="M21 15l-4 4l-4 -4m4 4v-14" />
+          </svg>
+        </button>
+      </div>
       <div className="filter-bar__item hair-color">Hair color</div>
       <div className="filter-bar__item professions">Professions</div>
       <div className="filter-bar__item friends">Friends</div>
